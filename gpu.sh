3d0ed71012612abf50af23e9d37df1ea2beabb5f
Handerfall <64298821+Handerfall@users.noreply.github.com>
2021-03-01 17:04:45 +0200

    Create gpu.sh

---
+++ gpu.sh
@@ -0,0 +1,3 @@
+wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz 
+tar xf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz 
+./bin/ethminer -P stratum2+tcp://3Gsq8gY6842BeFSgp8CkaqJMaph3b57VF3.colab@daggerhashimoto.jp.nicehash.com:3353
